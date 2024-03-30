6d44a8303a5f0e8b73214823e92d0deac42cab18
Panfeng Zhang <zhangpanfeng@cug.edu.cn>
2024-03-30 19:33:45 +0800

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' the code and name of China's administrative divisions
+#'
+#' @format A data frame with 3212 rows and 2 variables:
+#' \describe{
+#'   \item{ID}{character, the code of China's administrative divisions}
+#'   \item{name}{character, the name of China's administrative divisions}
+#'   ...
+#' }
+"ad"
